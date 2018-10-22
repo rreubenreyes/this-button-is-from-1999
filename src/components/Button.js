17078c244cac55dba2c3a66bf03dbc14524a9af7
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-10-22 15:34:45 -0700

    refactor: comments for self-documentation

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,6 +5,8 @@ import composeObject from '../helpers/composeObject'
 export default class Button extends Component {
   constructor() {
     super()
+
+    /* grab all possible keys from Memes object and load to state */
     const keys = Object.keys(Memes)
     const values = keys.map(() => ({ class: '' }))
     this.state = {
@@ -12,18 +14,26 @@ export default class Button extends Component {
       handleMouseOver: () => {},
       handleClick: () => {}
     }
+
+    /* to be used by handler functions on meme definition */
     this.button = createRef()
   }
 
   componentDidMount = () => {
+    /* set state to all random properties from Memes source object */
     const keys = Object.keys(Memes)
     const values = keys.map(meme => this.pickRandomMeme(Memes[meme]))
     this.setState(composeObject({ keys, values }))
   }
 
   pickRandomMeme = meme => {
+    /* only accept arrays */
     if (!meme.length) return { class: '' }
+
+    /* select a random meme whose props will be returned */
     const randomMeme = meme[Math.floor(Math.random() * meme.length)]
+
+    /* bind all handler functions to this component */
     const { handleMouseOver, handleClick } = randomMeme
     if (typeof handleMouseOver === 'function') {
       this.setState(() => ({
@@ -35,16 +45,18 @@ export default class Button extends Component {
         handleClick: handleClick.bind(this)
       }))
     }
+
     return randomMeme
   }
 
   render() {
+    /* grab class names from randomly selected memes */
     const randomClassNames = Object.values(this.state)
       .map(meme => meme.class)
       .join(' ')
 
+    /* grab handler functions from randomly selected memes */
     const { handleClick, handleMouseOver } = this.state
-    console.log(this.state)
 
     return (
       <button
