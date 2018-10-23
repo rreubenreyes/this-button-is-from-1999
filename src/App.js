0c2b12b557dace51b1e0335d684708d34c410ae3
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-10-23 09:17:04 -0700

    feat: add publication date counter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import Button from './components/Button'
 
 class App extends Component {
   render() {
+    const utcDays = date => date / (1000 * 60 * 60 * 24)
+    const publishDate = Math.floor(utcDays(Date.parse(`10/21/18`)))
+    const today = Math.floor(utcDays(Date.now()))
+
     return (
       <div className="app">
         <Button />
+        <span
+          style={{
+            marginTop: `1.5rem`,
+            width: `50%`
+          }}>
+          days since app has been deployed and it still sucks because reuben is
+          too lazy to add new shit to it: <strong>{today - publishDate}</strong>
+        </span>
       </div>
     )
   }
--- src/memes/onclick.js
+++ src/memes/onclick.js
@@ -1,9 +1,11 @@
+import dontPlayThisAtWork from './audio/dont-play-this-at-work.mp3'
+
 export default [
   {
     class: 'dont-play-this-at-work',
     _desc: 'see for yourself 😉',
     async handleClick() {
-      const audio = await new Audio('audio/dont-play-this-at-work.mp3')
+      const audio = await new Audio(dontPlayThisAtWork)
       console.log('hi')
       audio.play()
     }
